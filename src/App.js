8d07e5bbd659ae83b608f9cbcce7c1ef1e94c731
identity16 <dnjswns0930@gmail.com>
2022-01-15 19:34:53 +0900

    Intro Page(Mobile)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
+import { Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "./App.scss";
+import { useEffect, useState } from "react";
+import { getAuthor } from "./api/author";
+import IntroPage from "./pages/IntroPage";
 
 function App() {
-    return <div className="App"></div>;
+    const [author, setAuthor] = useState({
+        id: -1,
+        name: "",
+        featuredImages: [],
+    });
+
+    useEffect(() => {
+        setAuthor(getAuthor(1));
+    }, []);
+
+    return (
+        <div className="App">
+            <Swiper modules={[Navigation]} navigation>
+                <SwiperSlide>
+                    <IntroPage author={author} />
+                </SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+            </Swiper>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/IntroPage.js
@@ -0,0 +1,35 @@
+import Masonry from "react-masonry-css";
+import "./IntroPage.scss";
+
+function IntroPage({ author }) {
+    return (
+        <section className="IntroPage">
+            <h1>{author.name} 작가님</h1>
+            {author.featuredImages.length > 0 && (
+                <>
+                    <img
+                        className="featured-image"
+                        src={author.featuredImages[0].src}
+                        alt={author.featuredImages[0].description}
+                    />
+                    <Masonry
+                        breakpointCols={2}
+                        className="featured-image-list"
+                        columnClassName="featured-image-list_column"
+                    >
+                        {author.featuredImages.slice(1).map((fImage) => (
+                            <img
+                                className="featured-image"
+                                key={fImage.src}
+                                src={fImage.src}
+                                alt={fImage.description}
+                            />
+                        ))}
+                    </Masonry>
+                </>
+            )}
+        </section>
+    );
+}
+
+export default IntroPage;
